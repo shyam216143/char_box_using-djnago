37bf74f2222c6fbf68528fea8d0f88bff6797b91
shyam@216143 <vennushyamkumar@gmail.com>
2022-09-09 16:51:21 +0530

    updated

---
+++ chartroomProject/chartroomApp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+import threading
+# Create your views here.
+
+def index(request):
+    print(threading.get_native_id())
+    return render(request, 'chat/index.html')
+
+
+
+def room(request, room_name):
+    return render(request, 'chat/room.html', {
+        'room_name': room_name
+    })
